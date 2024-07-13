579fd26eb119a56cc9ca4c04fa2f6292c80ff920
apirrone <awanegenebayo@gmail.com>
2024-07-13 10:30:36 +0200

    commands update

--- legged_gym/envs/bdx/bdx_config.py
+++ legged_gym/envs/bdx/bdx_config.py
@@ -13,9 +13,9 @@ class BdxRoughCfg(LeggedRobotCfg):
 
     class commands(LeggedRobotCfg.commands):
         class ranges(LeggedRobotCfg.commands.ranges):
-            lin_vel_x = [-0.05, 0.05]  # min max [m/s]
-            lin_vel_y = [-0.05, 0.05]  # min max [m/s]
-            ang_vel_yaw = [-0.1, 0.1]  # min max [rad/s]
+            lin_vel_x = [-0.1, 0.1]  # min max [m/s]
+            lin_vel_y = [-0.1, 0.1]  # min max [m/s]
+            ang_vel_yaw = [-0.05, 0.05]  # min max [rad/s]
             heading = [-3.14, 3.14]
 
     class init_state(LeggedRobotCfg.init_state):
@@ -108,7 +108,7 @@ class BdxRoughCfg(LeggedRobotCfg):
             dof_vel = -0.0
             ang_vel_xy = -0.0
             feet_contact_forces = -0.0
-            head_behavior = 0.1
+            head_behavior = 0.05
 
 
 class BdxRoughCfgPPO(LeggedRobotCfgPPO):
